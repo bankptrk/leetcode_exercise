40edc060186221524e88148872b09270bf1bc28b
Pattarakorn Kittiworapanya <119297049+bankptrk@users.noreply.github.com>
2024-08-27 19:16:40 +0700

    Time: 15 ms (56.21%), Space: 15 MB (25.8%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+       stringstream ss(s);
+       string word, ans;
+       while(ss >> word){
+           reverse(word.begin(),word.end());
+           ans += word + " ";
+       }
+       return ans.substr(0,ans.size()-1); 
+    }
+};
