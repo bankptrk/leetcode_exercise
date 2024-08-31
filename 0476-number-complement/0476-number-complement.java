d20b3de1622d026b0f40adbba4fa7faad140bfe4
Pattarakorn Kittiworapanya <119297049+bankptrk@users.noreply.github.com>
2024-08-31 22:22:07 +0700

    Time: 0 ms (100%), Space: 40.6 MB (32.33%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,6 +1,7 @@
 class Solution {
     public int findComplement(int num) {
         if(num == 0) return 1;
+        if(num == 1) return 0;
         int bitLength = Integer.toBinaryString(num).length();
         int mask = (1 << bitLength)-1;
         return num ^ mask;
