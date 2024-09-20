f312a583faba39a9493f8da5a5d5bb33864626d1
Pattarakorn Kittiworapanya <119297049+bankptrk@users.noreply.github.com>
2024-09-20 22:21:27 +0700

    Time: 1 ms (76.61%), Space: 2.5 MB (76.38%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.go
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.go
@@ -1,17 +1,11 @@
 func uncommonFromSentences(s1 string, s2 string) []string {
     mpp := make(map[string]int)
-    var ans [] string
-    sentence1 := strings.Split(s1, " ")
-    sentence2 := strings.Split(s2, " ")
-
-    for _, sen := range sentence1{
+    var ans []string
+    for _, sen := range append(strings.Split(s1, " "), strings.Split(s2, " ")...){
         mpp[sen]++
     }
-    for _, sen := range sentence2{
-        mpp[sen]++
-    }
-    for sen, cnt := range mpp{
-        if cnt == 1{
+    for sen, val := range mpp{
+        if val == 1{
             ans = append(ans, sen)
         }
     }
